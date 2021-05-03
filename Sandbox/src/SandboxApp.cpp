19f3045d6e7d6cd7902d68d0678f2d9978458ebb
Palak Singh <rajputhpalak27@gmail.com>
2021-05-03 16:56:02 +0530

    Hazel Engine Entry Point

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,24 +1,15 @@
 #include <Hazel.h>
-#include <Hazel/Core/EntryPoint.h>
 
-#include "Sandbox2D.h"
-#include "ExampleLayer.h"
-
-class Sandbox : public Hazel::Application
-{
+class Sandbox :public Hazel::Application {
 public:
-	Sandbox()
-	{
-		// PushLayer(new ExampleLayer());
-		PushLayer(new Sandbox2D());
-	}
+	Sandbox() {
 
-	~Sandbox()
-	{
+	}
+	~Sandbox() {
+
 	}
 };
 
-Hazel::Application* Hazel::CreateApplication()
-{
+Hazel::Application* Hazel::CreateApplication() {
 	return new Sandbox();
 }
